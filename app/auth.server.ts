503df28b1f56a8ef10cb7e5fc98873b04ab36c11
Chris Carper <chris@descope.com>
2023-10-04 10:04:23 -0500

    removed hard coded values in auth.server.ts

--- app/auth.server.ts
+++ app/auth.server.ts
@@ -8,10 +8,9 @@ authenticator.use(
   new DescopeAuthenticator(
     {
       domain: `api.descope.com`,
-      clientID: "P2ShseKVGkaSg8x0lFizBkFuVbju",
-      clientSecret:
-        "K2WDcEFrPcPo1TFndBlO6CgC0AC6qIBoXoMd3PxfBKthX8ftESJBDtGDLXbfGu42Wv6U8RA",
-      callbackURL: "http://localhost:3000/auth/callback",
+      clientID: process.env.CLIENT_ID || "",
+      clientSecret: process.env.CLIENT_SECRET || "",
+      callbackURL: process.env.AUTH_CALLBACK_URL || "http://localhost:3000/auth/callback",
       scope: "openid profile email",
     },
     async ({ accessToken, refreshToken, extraParams, profile, context }) => {
