c4534f369ab7cd1f47361a19da24b9a100a4b498
Iddo Mayblum <iddo.mayblum@descope.com>
2024-06-25 09:22:11 -0700

    static var for baseurl

--- app/auth.server.ts
+++ app/auth.server.ts
@@ -3,16 +3,11 @@ import { DescopeAuthenticator } from "./DescopeAuthenticator.js";
 import { sessionStorage } from "~/session.server";
 
 export let authenticator = new Authenticator(sessionStorage);
-let baseURL = "api.descope.com"
-if (process.env.DESCOPE_PROJECT_ID.length >= 32) {
-  const localURL = process.env.DESCOPE_PROJECT_ID.substring(1, 5)
-  baseURL = [baseURL.slice(0, 4), localURL, ".", baseURL.slice(4)].join('') 
-}
 
 authenticator.use(
   new DescopeAuthenticator(
     {
-      domain: baseURL,
+      domain: DescopeAuthenticator.baseURL,
       clientID: process.env.CLIENT_ID || "",
       clientSecret: process.env.CLIENT_SECRET || "",
       callbackURL: process.env.AUTH_CALLBACK_URL || "http://localhost:3000/auth/callback",
--- app/routes/logout.tsx
+++ app/routes/logout.tsx
@@ -1,13 +1,9 @@
 import type { LoaderFunction } from "@remix-run/node";
 import { authenticator } from "~/auth.server";
+import { DescopeAuthenticator } from "~/DescopeAuthenticator";
 export let loader: LoaderFunction = async ({ request }) => {
 
-   let baseURL = "api.descope.com"
-   if (process.env.DESCOPE_PROJECT_ID.length >= 32) {
-      const localURL = process.env.DESCOPE_PROJECT_ID.substring(1, 5)
-      baseURL = [baseURL.slice(0, 4), localURL, ".", baseURL.slice(4)].join('') 
-   }
-   await fetch(`https://${baseURL}/oauth2/v1/logout`);
+   await fetch(`https://${DescopeAuthenticator.baseURL}/oauth2/v1/logout`);
 
    await authenticator.logout(request, { redirectTo: "/" });
   };
